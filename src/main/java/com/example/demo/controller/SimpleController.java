ef1df708a0015fd08f8d70124a3be8fdf5997054
Yahya Baassou <yahya.baassou@zenika.com>
2019-03-14 15:00:17 +0800

    creating simple controllers

---
+++ src/main/java/com/example/demo/controller/SimpleController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+
+}
